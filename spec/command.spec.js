35b3d4e171aad20c8d0b7227c81324e9c807e894
Hannah Overton <hannahoverton99@gmail.com>
2024-04-17 11:38:24 -0500

    Task 2 through test 3

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -4,17 +4,28 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Command class", function() {
-
-  it("throws error if command type is NOT passed into constructor as the first parameter", function() {
+//TEST 1
+test("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
 
-
-  it("constructor sets command type", function(){
-  });
+//TEST 2
+  test("constructor sets command type", function(){
+    let attempt = function(){new Command(commandType, value)
+      this.commandType= commandType;
+      }
+      expect(attempt);
+    });
   
-  it("constructor sets a value passed in as the 2nd argument", function(){
-  expect( function() { new Command(commandtype, value)})
+  
+//TEST 3
+  test("constructor sets a value passed in as the 2nd argument", function(){
+    let attempt= function(){
+    new Command(commandType, value)
+    commandType= this.commandType;
+    value=this.value;
+  };
+    expect(attempt)
   });
-});
 
+});
